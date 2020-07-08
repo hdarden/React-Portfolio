8ed36f808cb11310dc3bffa9935dc0b9794a5f03
Helen Darden <hgdarden@gmail.com>
2020-07-08 09:51:14 -0400

    Updates

---
+++ precache-manifest.26c98debb4a0961e1310fbfdc2e06154.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46c0cfde79539b9385c0aa76ce2b9845",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "4b66aedfb95d8aaeaece",
+    "url": "/React-Portfolio/static/css/main.a95ff25a.chunk.css"
+  },
+  {
+    "revision": "b272b1f160c79e49b9ad",
+    "url": "/React-Portfolio/static/js/2.01277a97.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Portfolio/static/js/2.01277a97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b66aedfb95d8aaeaece",
+    "url": "/React-Portfolio/static/js/main.64529d78.chunk.js"
+  },
+  {
+    "revision": "bcebe2d38e8b197ca4b9",
+    "url": "/React-Portfolio/static/js/runtime-main.266d4783.js"
+  },
+  {
+    "revision": "deae5706df768e73f79c5e902a9e05c0",
+    "url": "/React-Portfolio/static/media/3F433DBF-C089-4B85-933E-32AD8C43F3F7_1_201_a.deae5706.jpeg"
+  },
+  {
+    "revision": "68fa4c7808ca11238a405a431a5dcea4",
+    "url": "/React-Portfolio/static/media/50C6DB0C-FFAE-4748-9457-069AD94477FC_1_201_a.68fa4c78.jpeg"
+  },
+  {
+    "revision": "1f98bd1776197c197e9343ab11872dbb",
+    "url": "/React-Portfolio/static/media/6599367E-EA27-4924-B251-221BBE154ADA_1_201_a.1f98bd17.jpeg"
+  },
+  {
+    "revision": "fce90407767498c60a5e9bb753b3c48c",
+    "url": "/React-Portfolio/static/media/77C7FF12-F910-4C62-95A0-9B6EF434A5AA_1_105_c.fce90407.jpeg"
+  },
+  {
+    "revision": "aa68cd00e4164d00d504b7c48832aafb",
+    "url": "/React-Portfolio/static/media/E2755C25-CD8F-4A55-A111-CD8AC7C4D88F_1_201_a.aa68cd00.jpeg"
+  },
+  {
+    "revision": "adf22a75918863e04aae0bb19475ef22",
+    "url": "/React-Portfolio/static/media/F9749EA8-F884-4019-9E74-06F6AFF29217_1_105_c.adf22a75.jpeg"
+  },
+  {
+    "revision": "395c7b7b56f564d2a57a094672c5aadb",
+    "url": "/React-Portfolio/static/media/FullSizeRender.395c7b7b.jpeg"
+  },
+  {
+    "revision": "1d70d4e9ad00e3376624338915b96d57",
+    "url": "/React-Portfolio/static/media/Portfolio Resume.1d70d4e9.pdf"
+  }
+]);
